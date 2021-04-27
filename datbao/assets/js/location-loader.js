02e362d1e2e220ce9045cad8dbff7804f51ba716
lamtd <lamtd@boonygroup.com>
2021-04-27 14:35:37 +0700

    fixed

--- datbao/assets/js/location-loader.js
+++ datbao/assets/js/location-loader.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
     _load_city_to_select();
 });
 function _load_city_to_select(){
-    $.getJSON( "http://localhost:8000/api/city", function( data ) {
+    $.getJSON( "https://public.forbes.vn/api/city", function( data ) {
       $.each( data, function( key, val ) {
         $("select#city_id").append($('<option value="' + val['city_id'] + '">' + val['city_name'] + '</option>'));
       });
     });
   }
 function _load_district_to_select(){
-    var _url = 'http://localhost:8000/api/city/' + $("select#city_id").val();
+    var _url = 'https://public.forbes.vn/api/city/' + $("select#city_id").val();
     $("select#district_id").empty();
     $.getJSON( _url, function( data ) {
       $.each( data, function( key, val ) {
@@ -18,7 +18,7 @@ function _load_district_to_select(){
     });
   }
 function _load_ward_to_select(){
-    var _url = 'http://localhost:8000/api/district/' + $("select#district_id").val();
+    var _url = 'https://public.forbes.vn/api/district/' + $("select#district_id").val();
     $("select#ward_id").empty();
     $.getJSON( _url, function( data ) {
       $.each( data, function( key, val ) {
