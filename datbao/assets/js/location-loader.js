d6ddbbfba365259e9ad2dd4502d00fe403c16d22
lamtd <lamtd@boonygroup.com>
2021-04-27 16:28:01 +0700

    Update location-loader.js

--- datbao/assets/js/location-loader.js
+++ datbao/assets/js/location-loader.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     _load_city_to_select();
 });
 function _load_city_to_select(){
-    $.getJSON( "http://localhost:8000/api/city", function( data ) {
+    $.getJSON( "https://public.forbes.vn//api/city", function( data ) {
       $("select#city_id").append('<option value="">--- Thành phố ---</option>');
       $.each( data, function( key, val ) {
         $("select#city_id").append($('<option value="' + val['city_id'] + '">' + val['city_name'] + '</option>'));
@@ -10,7 +10,7 @@ function _load_city_to_select(){
     });
   }
 function _load_district_to_select(){
-    var _url = 'http://localhost:8000/api/city/' + $("select#city_id").val();
+    var _url = 'https://public.forbes.vn//api/city/' + $("select#city_id").val();
     $("select#district_id").empty();
     $("select#district_id").append('<option value="">--- Quận / Huyện ---</option>');
     $.getJSON( _url, function( data ) {
@@ -20,7 +20,7 @@ function _load_district_to_select(){
     });
   }
 function _load_ward_to_select(){
-    var _url = 'http://localhost:8000/api/district/' + $("select#district_id").val();
+    var _url = 'https://public.forbes.vn//api/district/' + $("select#district_id").val();
     $("select#ward_id").empty();
     $("select#ward_id").append('<option value="">--- Phường / Xã ---</option>');
     $.getJSON( _url, function( data ) {
